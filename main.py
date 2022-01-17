4db1d8dede615cd9c0a8a88db3173d7521931f66
promel <jkm.more@gmail.com>
2022-01-17 09:41:28 +0200

    color filli

--- main.py
+++ main.py
@@ -30,7 +30,12 @@ for lt, ln, el in zip(lat, lon, elev):
     # iframe = folium.IFrame(html=html % str(el), width=200, height=100)
     # fg.add_child(folium.Marker(location=[lt,ln],popup=iframe,icon=folium.Icon(color='green')))
     # fg.add_child(folium.Marker(location=[lt,ln],popup=str(el)+'m' ,icon=folium.Icon(color= color_producer(el))))
-    fg.add_child(folium.CircleMarker(location=[lt, ln], radius=6, popup=str(el)+'m', fill_color=color_producer(el), color='grey', fill_opacity=0.7))
-fg.add_child(folium.GeoJson(data=open('world.json','r', encoding='utf-8-sig').read()))
+    fg.add_child(folium.CircleMarker(location=[lt, ln], radius=6, popup=str(
+        el)+'m', fill_color=color_producer(el), color='grey', fill_opacity=0.7))
+
+fg.add_child(folium.GeoJson(data=open('world.json', 'r',
+                                      encoding='utf-8-sig').read(),
+                            style_function=lambda x: {'fillColor':'green' if x['properties']['POP2005'] < 10000000
+                            else 'orange' if 10000000 <= x['properties']['POP2005'] < 20000000 else 'red'}))
 map.add_child(fg)
 map.save("Map1.htm")
